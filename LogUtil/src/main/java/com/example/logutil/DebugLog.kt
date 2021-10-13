3f424b64d41abd0adab2931048d2521a4f4c94f7
ayaigi <86873009+ayaigi@users.noreply.github.com>
2021-10-13 19:50:02 +0200

    v0.1.6

--- LogUtil/src/main/java/com/example/logutil/DebugLog.kt
+++ LogUtil/src/main/java/com/example/logutil/DebugLog.kt
@@ -1,12 +1,14 @@
 package com.example.logutil
 
 import android.util.Log
+import com.example.lib.Degrees
 
 class DebugLog {
     companion object {
         const val TAG = "AppDebug"
     }
     fun d(message: String){
+        Degrees()
         Log.d(TAG, message)
     }
 }
